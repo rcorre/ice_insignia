f766a7078dee5cd3ae2cd758ec5a1710967e0ac0
Ryan RC <murphyslaw480@gmail.com>
2014-08-21 20:20:31 -0400

    color lerp

--- src/graphics/color.d
+++ src/graphics/color.d
@@ -30,36 +30,51 @@ ALLEGRO_COLOR lerp(ALLEGRO_COLOR start, ALLEGRO_COLOR end, float factor) {
   return color(r, g, b, a);
 }
 
+ALLEGRO_COLOR lerp(ALLEGRO_COLOR[] colors, float factor) {
+  assert(colors.length > 2);
+  float colorTime = 1.0 / (colors.length - 1); // time for each color pair
+  int idx = roundDown(factor * (colors.length - 1));
+  if (idx < 0) {  // before first color
+    return colors[0];  // return first
+  }
+  else if (idx >= colors.length - 1) {  // past last color
+    return colors[$ - 1];  // return last
+  }
+  factor = (factor % colorTime) / colorTime; 
+  return lerp(colors[idx], colors[idx + 1], factor);
+}
+
 unittest {
-  import std.math : approxEqual;
+  bool approxEqual(ALLEGRO_COLOR c1, ALLEGRO_COLOR c2) {
+    import std.math : approxEqual;
+    return c1.r.approxEqual(c2.r) &&
+      c1.g.approxEqual(c2.g) &&
+      c1.b.approxEqual(c2.b) &&
+      c1.a.approxEqual(c2.a);
+  }
+
   // float color with implied alpha
   auto c1 = color(0.5, 1, 0.3);
-  assert(c1.r.approxEqual(0.5f));
-  assert(c1.g.approxEqual(1.0f));
-  assert(c1.b.approxEqual(0.3f));
-  assert(c1.a.approxEqual(1.0f));
+  assert(approxEqual(c1, ALLEGRO_COLOR(0.5, 1, 0.3, 1)));
 
   // float color with specified alpha
   auto c2 = color(0, 0, 0, 0.5);
-  assert(c2.r.approxEqual(0));
-  assert(c2.g.approxEqual(0));
-  assert(c2.b.approxEqual(0));
-  assert(c2.a.approxEqual(0.5f));
+  assert(approxEqual(c2, ALLEGRO_COLOR(0, 0, 0, 0.5)));
 
   // unsigned color with implied alpha
   auto c3 = ucolor(100, 255, 0);
-  assert(c3.r.approxEqual(100 / 255f));
-  assert(c3.g.approxEqual(255 / 255f));
-  assert(c3.b.approxEqual(  0 / 255f));
-  assert(c3.a.approxEqual(255 / 255f));
+  assert(approxEqual(c3, ALLEGRO_COLOR(100 / 255f, 1, 0, 1)));
 
   // unsigned color with specified alpha
   auto c4 = ucolor(0, 0, 255, 127);
-  assert(c4.r.approxEqual(  0 / 255f));
-  assert(c4.g.approxEqual(  0 / 255f));
-  assert(c4.b.approxEqual(255 / 255f));
-  assert(c4.a.approxEqual(127 / 255f));
+  assert(approxEqual(c4, ALLEGRO_COLOR(0, 0, 1, 127 / 255f)));
 
   auto c5 = Tint.black.lerp(Tint.white, 0.5);
   assert(c5 == color(0.5, 0.5, 0.5, 1));
+
+  assert(lerp([Tint.black, Tint.white, Tint.red], 0) == Tint.black);
+  assert(lerp([Tint.black, Tint.white, Tint.red], 0.1) == color(0.2, 0.2, 0.2));
+  assert(lerp([Tint.black, Tint.white, Tint.red], 0.5) == Tint.white);
+  assert(approxEqual(lerp([Tint.black, Tint.white, Tint.red], 0.6), color(1, 0.8, 0.8)));
+  assert(approxEqual(lerp([Tint.black, Tint.white, Tint.red], 1), Tint.red));
 }
