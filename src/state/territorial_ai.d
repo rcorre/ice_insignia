0183350add105ed78e97913751fd96bda266dd02
Ryan RC <murphyslaw480@gmail.com>
2014-08-27 08:52:18 -0400

    separate territorial and agressive ai

--- src/state/territorial_ai.d
+++ src/state/territorial_ai.d
@@ -1,15 +1,14 @@
-module state.agressive_ai;
+module state.territorial_ai;
 
 import std.algorithm;
 import std.range;
 import std.math;
 import state.ai;
-import state.territorial_ai;
 import model.battler;
 import tilemap.all;
 
-/// move towards closest opponent regardless of how far away they are
-class AgressiveAI : TerritorialAI {
+/// attack enemies that enter move range -- otherwise skip turn
+class TerritorialAI : Behavior {
   this(Battler self, TileMap map, Battler[] enemies, Battler[] allies) {
     super(self, map, enemies, allies);
   }
@@ -19,23 +18,25 @@ class AgressiveAI : TerritorialAI {
       if (attackRequest !is null) {
         return null;  // don't move if target in range
       }
-      if (super.moveRequest) { // if enemy in territory, move to it
-        return super.moveRequest;
-      }
 
       // find closest enemy
       int distTo(Battler other) { return abs(_self.row - other.row) + abs(_self.col - other.col); }
       // find nearest living target
       auto target = _enemies.sort!((a,b) => distTo(a) < distTo(b)).find!"a.alive".front;
       auto tiles = _map.neighbors(_map.tileAt(target.row, target.col));
-      // route towards nearest enemy
+
       foreach(tile ; tiles) {
-        auto path = _pathFinder.pathToward(tile);
+        auto path = _pathFinder.pathTo(tile);
         if (path) {
           return path; // return path up to but not including target's tile
         }
       }
-      return null;
+      return null; // no enemy in move range
+    }
+
+    Battler attackRequest() {
+      auto result = _enemies.find!(x => _self.canAttack(x));
+      return result.empty ? null : result.front;
     }
   }
 }
