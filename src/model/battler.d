083e4be98ab7da2c3376f6e28a7b6393284d3249
Ryan RC <murphyslaw480@gmail.com>
2014-08-30 17:22:21 -0400

    use character alias in battler

--- src/model/battler.d
+++ src/model/battler.d
@@ -26,10 +26,10 @@ enum BattleTeam {
 }
 
 class Battler {
-  //alias character this;
+  alias character this;
 
   this(Character c, int row, int col, Vector2i pos, Sprite sprite, BattleTeam team, string aiType = "agressive") {
-    _character = c;
+    character = c;
     _row = row;
     _col = col;
     _pos = pos;
@@ -44,7 +44,6 @@ class Battler {
     ref int row() { return _row; }
     ref int col() { return _col; }
     ref Vector2i pos() { return _pos; }
-    //Character character() { return _character; }
     int hp() { return _hp; }
 
     bool alive() { return _hp > 0; }
@@ -62,14 +61,6 @@ class Battler {
     bool isHpTransitioning() { return _infoBox.healthBar.isTransitioning; }
 
     string aiType() {return _aiType; }
-
-    // HACK due to alias this bug
-    auto equippedWeapon() {
-      return _character.equippedWeapon;
-    }
-    void equippedWeapon(Item item) {
-      _character.equippedWeapon = item;
-    }
   }
 
   void update(float time) {
@@ -88,7 +79,7 @@ class Battler {
 
   void showInfoBox(Vector2i pos) {
     if (_infoBox is null) {
-      _infoBox = new BattlerInfoBox(pos, _character.name, _hp, _character.maxHp);
+      _infoBox = new BattlerInfoBox(pos, character.name, _hp, character.maxHp);
     }
   }
 
@@ -114,21 +105,16 @@ class Battler {
 
   bool canAttack(Battler other) {
     auto dist = abs(row - other.row) + abs(col - other.col);
-    return other.alive && dist >= _character.equippedWeapon.minRange && dist <= _character.equippedWeapon.maxRange;
-  }
-
-  /// access an attribute by name
-  auto opDispatch(string m)() {
-    return mixin("_character." ~ m);
+    return other.alive && dist >= character.equippedWeapon.minRange && dist <= character.equippedWeapon.maxRange;
   }
 
   const BattleTeam team;
+  Character character;
 
   private:
   Sprite _sprite;
   int _row, _col;
   Vector2i _pos;
-  Character _character;
   int _hp;
   bool _moved;
   BattlerInfoBox _infoBox;
