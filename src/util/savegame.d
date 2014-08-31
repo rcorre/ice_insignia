98aa3f74042a841ecafb60061ed5047e9f2cd094
Ryan RC <murphyslaw480@gmail.com>
2014-08-31 18:29:39 -0400

    save data setup

---
+++ src/util/savegame.d
@@ -0,0 +1,47 @@
+// TODO: untested
+module util.savegame;
+
+import std.file;
+import std.path;
+import model.character;
+import util.jsonizer;
+
+private string fileName = "/ice_insignia_save.json";
+
+class SaveData {
+  mixin JsonizeMe;
+  @jsonize {
+    Character[] roster;
+    int gold;
+    int mission;
+  }
+}
+
+SaveData loadSave() {
+  if (!savePath.exists) { return null; }
+  return savePath.readJSON!SaveData;
+}
+
+void saveGame(Character[] roster, int gold, int mission) {
+  SaveData data;
+  data.roster = roster;
+  data.gold = gold;
+  data.mission = mission;
+  data.writeJSON(savePath);
+}
+
+private @property string savePath() {
+  string dir;
+  version(Windows) {
+    dir = "./savegame";
+  }
+  version(linux) {
+    dir = expandTilde("~/.ice_insignia");
+  }
+
+  if (!dir.exists) {
+    mkdir(dir);
+  }
+
+  return dir ~ fileName;
+}
