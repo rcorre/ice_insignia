d7c47255d6f8ca3b0abc50cdc9768fdb4d6e7446
Ryan RC <murphyslaw480@gmail.com>
2014-09-05 12:45:52 -0400

    support different animation repeat types

--- src/graphics/animation.d
+++ src/graphics/animation.d
@@ -7,24 +7,49 @@ import graphics.sprite;
 import graphics.color;
 
 class AnimatedSprite : Sprite {
-  this(string name, Color tint = Color.white) {
-    super(name, tint);
+  // TODO: replace no with stop (stop animating) or destroy (remove sprite)
+  enum Repeat {
+    unspecified, /// use value from config file
+    no,          /// run only once
+    loop,        /// loop back to beginning
+    rebound      /// reverse animation direction
+  }
 
+  this(string name, Color tint = Color.white, Repeat repeat = Repeat.unspecified) {
+    super(name, tint);
     auto data = _animationData.entries[name];
     _startCol = _col;
     _endCol = _col + to!int(data["length"]);
     _frameTime = to!float(data["frameTime"]);
+    if (repeat == Repeat.unspecified) { // try to load repeat setting from config
+      repeat = to!Repeat(data.get("repeat", "no")); // default to Repeat.no if not found
+    }
+    _repeat = repeat;
     _timer = _frameTime;
   }
 
   override void update(float time) {
     super.update(time);
+    if (!_animate) { return; }
+
     _timer -= time;
     if (_timer < 0) {
       _timer = _frameTime;
       ++_col;
       if (_col > _endCol) {
-        _col = _startCol;
+        final switch(_repeat) with(Repeat) {
+          case no:
+            _animate = false;
+            _col = _endCol;
+            break;
+          case loop:
+            _col = _startCol;
+            break;
+          case rebound:
+            assert(0, "AnimatedSprite.Repeat.rebound not implemented");
+          case unspecified:
+            assert(0, "AnimatedSprite.Repeat should never be unspecified");
+        }
       }
     }
   }
@@ -32,6 +57,8 @@ class AnimatedSprite : Sprite {
   private:
   float _timer, _frameTime;
   int _startCol, _endCol;
+  Repeat _repeat;
+  bool _animate = true;
 }
 
 private ConfigData _animationData;
