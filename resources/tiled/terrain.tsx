5d5315b7baabcf267df3ab3802ddbc8d9a3217aa
Ryan RC <murphyslaw480@gmail.com>
2014-09-09 13:01:35 -0400

    make first map

---
+++ resources/tiled/terrain.tsx
@@ -0,0 +1,168 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="terrain" tilewidth="32" tileheight="32">
+ <image source="../../content/image/terrain.png" width="320" height="320"/>
+ <tile id="0">
+  <properties>
+   <property name="name" value="Field"/>
+  </properties>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="moveCost" value="20"/>
+   <property name="name" value="Water"/>
+  </properties>
+ </tile>
+ <tile id="10">
+  <properties>
+   <property name="avoid" value="20"/>
+   <property name="cavalryCost" value="3"/>
+   <property name="defense" value="2"/>
+   <property name="moveCost" value="2"/>
+   <property name="name" value="Forest"/>
+  </properties>
+ </tile>
+ <tile id="11">
+  <properties>
+   <property name="avoid" value="30"/>
+   <property name="cavalryCost" value="6"/>
+   <property name="defense" value="3"/>
+   <property name="moveCost" value="3"/>
+   <property name="name" value="Hill"/>
+  </properties>
+ </tile>
+ <tile id="21">
+  <properties>
+   <property name="moveCost" value="20"/>
+   <property name="name" value="Wall"/>
+  </properties>
+ </tile>
+ <tile id="22">
+  <properties>
+   <property name="moveCost" value="20"/>
+   <property name="name" value="Wall"/>
+  </properties>
+ </tile>
+ <tile id="23">
+  <properties>
+   <property name="moveCost" value="20"/>
+   <property name="name" value="Wall"/>
+  </properties>
+ </tile>
+ <tile id="24">
+  <properties>
+   <property name="moveCost" value="20"/>
+   <property name="name" value="Wall"/>
+  </properties>
+ </tile>
+ <tile id="31">
+  <properties>
+   <property name="moveCost" value="20"/>
+   <property name="name" value="Wall"/>
+  </properties>
+ </tile>
+ <tile id="32">
+  <properties>
+   <property name="name" value="Stone"/>
+  </properties>
+ </tile>
+ <tile id="33">
+  <properties>
+   <property name="name" value="Stone"/>
+  </properties>
+ </tile>
+ <tile id="34">
+  <properties>
+   <property name="moveCost" value="20"/>
+   <property name="name" value="Wall"/>
+  </properties>
+ </tile>
+ <tile id="35">
+  <properties>
+   <property name="name" value="Shore"/>
+  </properties>
+ </tile>
+ <tile id="36">
+  <properties>
+   <property name="name" value="Shore"/>
+  </properties>
+ </tile>
+ <tile id="37">
+  <properties>
+   <property name="name" value="Shore"/>
+  </properties>
+ </tile>
+ <tile id="38">
+  <properties>
+   <property name="name" value="Shore"/>
+  </properties>
+ </tile>
+ <tile id="39">
+  <properties>
+   <property name="name" value="Shore"/>
+  </properties>
+ </tile>
+ <tile id="41">
+  <properties>
+   <property name="moveCost" value="20"/>
+   <property name="name" value="Wall"/>
+  </properties>
+ </tile>
+ <tile id="42">
+  <properties>
+   <property name="moveCost" value="20"/>
+   <property name="name" value="Wall"/>
+  </properties>
+ </tile>
+ <tile id="43">
+  <properties>
+   <property name="moveCost" value="20"/>
+   <property name="name" value="Door"/>
+  </properties>
+ </tile>
+ <tile id="44">
+  <properties>
+   <property name="moveCost" value="20"/>
+   <property name="name" value="Wall"/>
+  </properties>
+ </tile>
+ <tile id="45">
+  <properties>
+   <property name="name" value="Shore"/>
+  </properties>
+ </tile>
+ <tile id="46">
+  <properties>
+   <property name="name" value="Shore"/>
+  </properties>
+ </tile>
+ <tile id="47">
+  <properties>
+   <property name="name" value="Shore"/>
+  </properties>
+ </tile>
+ <tile id="48">
+  <properties>
+   <property name="name" value="Shore"/>
+  </properties>
+ </tile>
+ <tile id="49">
+  <properties>
+   <property name="name" value="Shore"/>
+  </properties>
+ </tile>
+ <tile id="57">
+  <properties>
+   <property name="name" value="Shore"/>
+  </properties>
+ </tile>
+ <tile id="58">
+  <properties>
+   <property name="name" value="Shore"/>
+  </properties>
+ </tile>
+ <tile id="59">
+  <properties>
+   <property name="name" value="Shore"/>
+  </properties>
+ </tile>
+</tileset>
