4ded20e2eb52bf7e8ca035f490737dfb4f3d197e
Ryan RC <murphyslaw480@gmail.com>
2014-09-11 16:09:24 -0400

    draw text with button sprite

--- src/gui/button_sprites.d
+++ src/gui/button_sprites.d
@@ -3,12 +3,29 @@ module gui.button_sprites;
 import geometry.all;
 import graphics.all;
 
-void drawButtonSprite(string button, Vector2i pos) {
+private enum {
+  // text rect roundness
+  rx = 1,
+  ry = 1,
+  offsetX = 5
+}
+
+void drawButtonSprite(string button, Vector2i pos, string cmd = null) {
   assert(button in _sprites, "no button sprite named " ~ button);
-  _sprites[button].draw(pos);
+  auto sprite = _sprites[button];
+  if (cmd) {
+    auto width = _font.widthOf(cmd);
+    auto height = _font.heightOf(cmd);
+    auto area = Rect2i(pos.x + sprite.width / 2 - offsetX, pos.y - height / 2, width, height);
+    area.drawFilled(Color.black, 1, 1);
+    auto textPos = area.topLeft;
+    _font.draw(cmd, textPos, Color.white);
+  }
+  sprite.draw(pos);
 }
 
 private static Sprite[string] _sprites;
+private static Font _font;
 
 static this() {
   _sprites["a"]     = new Sprite("button_a");
@@ -21,4 +38,6 @@ static this() {
   _sprites["rt"]    = new Sprite("button_rt");
   _sprites["start"] = new Sprite("button_start");
   _sprites["back"]  = new Sprite("button_back");
+
+  _font = getFont("buttonSprite");
 }
