d199ea4c0bfbe38ac91342ba8151f9c71be67920
Ryan RC <murphyslaw480@gmail.com>
2014-09-25 21:46:53 -0400

    use sample instance for bg music

--- src/util/music.d
+++ src/util/music.d
@@ -6,6 +6,7 @@ import allegro;
 import util.config;
 
 alias Sample = ALLEGRO_SAMPLE*;
+alias SampleInstance = ALLEGRO_SAMPLE_INSTANCE*;
 
 void setMusicMute(bool mute) {
   _muted = mute;
@@ -20,17 +21,26 @@ void setMusicMute(bool mute) {
 // TODO: use audio streams?
 void playBgMusic(string key) {
   if (_muted) { return; }
+  if (_instance !is null) {
+    al_destroy_sample_instance(_instance);
+  }
   if (_musicSample !is null) {
     al_destroy_sample(_musicSample);  // also stops playing the sample
   }
   assert(key in _musicPaths, "no music for key " ~ key);
   _musicSample = al_load_sample(_musicPaths[key].toStringz);
   assert(_musicSample !is null, "failed to load music sample " ~ key);
-  al_play_sample(_musicSample, _gain, 0f, 1f, ALLEGRO_PLAYMODE.ALLEGRO_PLAYMODE_LOOP, &_id);
+  _instance = al_create_sample_instance(_musicSample);
+  assert(_instance !is null, "failed to load music instance for " ~ key);
+  al_set_sample_instance_playmode(_instance, ALLEGRO_PLAYMODE.ALLEGRO_PLAYMODE_LOOP);
+  al_attach_sample_instance_to_mixer(_instance, al_get_default_mixer());
+  al_play_sample_instance(_instance);
+  //al_play_sample_instance(_musicSample, _gain, 0f, 1f, ALLEGRO_PLAYMODE.ALLEGRO_PLAYMODE_LOOP, &_id);
 }
 
 private:
 Sample _musicSample;
+SampleInstance _instance;
 string[string] _musicPaths;
 float _gain;
 bool _muted;
@@ -50,5 +60,8 @@ static this() {
 static ~this() {
   if (_musicSample !is null) {
     al_destroy_sample(_musicSample);
+  }
+  if (_instance !is null) {
+    al_destroy_sample_instance(_instance);
   }
 }
